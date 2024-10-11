7dd72b42bc8e3028e545cdc6b20a3b407ce783ab
Soumyanetrap <soumyanetrap@gmail.com>
2024-10-11 06:51:47 +0530

    CORS modified 4

--- home.js
+++ home.js
@@ -53,7 +53,7 @@ const allowedOrigins = [
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     next();
   })
