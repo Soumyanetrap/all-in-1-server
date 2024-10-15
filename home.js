7b5e0ccc67d15e4d7f20412890fe350c9532aa07
Soumyanetrap <soumyanetrap@gmail.com>
2024-10-15 08:56:10 +0530

    ws rectified

--- home.js
+++ home.js
@@ -1,69 +1,25 @@
 const express = require('express');
-const WebSocket = require('ws');
 const cors = require('cors');
 const { client, connect } = require('./database/db');
 const sendOTP = require('./utils/send_mail');
-const logger = require('./utils/logger'); // Import the logger
+const logger = require('./utils/logger');
 const path = require('path');
-const authRoutes = require('./auth/authentication'); // Import the authentication routes
-const adminRoutes = require('./admin/db_operations'); // Import the admin routes
-const bankRoutes = require('./banks/bank_operations'); // Import the bank routes
-const ticketRoutes = require('./tickets/ticket_operations'); // Import the ticket routes
-const connRoutes = require('./connections/connections'); // Import the connection routes
-const storageRoutes = require('./storage/storage'); // Import the storage routes
+const authRoutes = require('./auth/authentication');
+const adminRoutes = require('./admin/db_operations');
+const bankRoutes = require('./banks/bank_operations');
+const ticketRoutes = require('./tickets/ticket_operations');
+const connRoutes = require('./connections/connections');
+const storageRoutes = require('./storage/storage');
 const tripRoutes = require('./trip/trip_operations');
-const chatRoutes = require('./chat/chat_operations'); // Import the chat routes
+const chatRoutes = require('./chat/chat_operations');
 
-const { authenticate, uploadFile, readFile } = require('./storage/storage_utils');
-
-const fileId = '1qXfVuz0UOVqYXf4kujDis3XgfBFxrgSr';
-
-// Function to authenticate and then read the file
-async function processFile() {
-    try {
-        // Authenticate and get the auth client
-        const authClient = await authenticate();
-        
-        // Read the file with the given file ID
-        const fileContent = await readFile(authClient, fileId);
-        console.log('File Content:', fileContent);
-    } catch (error) {
-        console.error('Error:', error.message);
-    }
-}
+const WebSocket = require('ws'); // Import WebSocket
 
 const app = express();
-
-// app.use((req, res, next) => {
-//     console.log(`Incoming request: ${req.method} ${req.url}`);
-//     console.log(`Request headers: ${JSON.stringify(req.headers)}`);
-//     next();
-// });
-
-
-// const allowedOrigins = [
-//     'http://localhost:3000',  // Your React app URL
-//     'https://your-production-url.com' // Add your production URL here
-// ];
+const port = process.env.PORT || 4000;
 
 // CORS Configuration
 app.use(cors());
-// app.use(cors({
-//     origin: function(origin, callback) {
-//         // Allow requests with no origin (like mobile apps or curl requests)
-//         if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-//             callback(null, origin);
-//         } else {
-//             callback(new Error('Not allowed by CORS'));
-//         }
-//     },
-//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-//     allowedHeaders: ['Content-Type', 'Authorization'],
-//     credentials: true,
-// }));
-
-// Enable preflight across all routes
-// app.options('*', cors()); // Enable preflight across all routes
 
 // Serve static files from the React app's build directory
 app.use(express.static(path.join(__dirname, './build')));
@@ -73,52 +29,29 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './build', 'index.html'));
 });
 
-// Handle any other routes
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, './build', 'index.html'));
-// });
-
-const port = process.env.PORT || 4000;
-
-const MAX_RETRIES = 5;
-const RETRY_DELAY = 5000; // 5 seconds
-
-// const server =
-    
-app.listen(port, () => {
+const server = app.listen(port, () => {
     logger.log(`Server is running on port ${port}`);
+    console.log(`WebSocket server URL: ws://localhost:${port}`);
 });
 
-// const http = require('http');
-// const server = http.createServer(app);
-// console.log(server)
-// const wssUrl = `ws://${server.address().address}:${server.address().port}`;
-// console.log(`WebSocket URL: ${wssUrl}`); 
+// Create WebSocket server
+const wss = new WebSocket.Server({ port: 4001 }); // Use a different port for WebSocket
 
-// const wss = new WebSocket.Server({ server });
-const wss = new WebSocket.Server({ port: 5000 }); 
-// Create an HTTP server
-// const server = http.createServer(app); 
-
-// Create a WebSocket server
-// const wss = new WebSocket.Server({ server });
-
-wss.on('error', (error) => {
-    console.error('WebSocket error:', error);
-});
-
-
+// Store connections and associated trip IDs
 const client_trips = new Map();
 
 wss.on('connection', (ws, req) => {
     logger.log('Client connected');
+    // console.log(req.url)
     const urlParams = new URLSearchParams(req.url.split('?')[1]);
     const tripId = urlParams.get('trip_id');
-
+    // console.log(tripId)
+    // console.log(ws)
     if (tripId) {
         // Store the WebSocket connection with the user ID
-        client_trips.set(ws, tripId);
+        client_trips.set(ws, parseInt(tripId, 10)); 
     }
+    // console.log(client_trips.keys())
 
     ws.on('message', (message) => {
         console.log('Received message:', message);
@@ -136,43 +69,45 @@ wss.on('connection', (ws, req) => {
 });
 
 async function connectToDatabase() {
+    const MAX_RETRIES = 5;
+    const RETRY_DELAY = 5000; // 5 seconds
     let attempt = 0;
+
     while (attempt < MAX_RETRIES) {
         try {
             const connected = await connect();
             if (connected) {
                 logger.log('Database connected successfully.');
-                console.log('Database connected successfully.')
+                console.log('Database connected successfully.');
                 try {
                     // Listen to both channels
                     await client.query('LISTEN req_con_changes');
                     await client.query('LISTEN new_chat');
 
                     client.on('notification', (msg) => {
-                        // Handle notifications based on the channel
                         if (msg.channel === 'req_con_changes') {
                             logger.log(`Change detected on req_con_changes: ${msg.payload}`);
-                            // Broadcast to WebSocket clients
+                            // Broadcast to all connected clients
                             wss.clients.forEach((client) => {
                                 if (client.readyState === WebSocket.OPEN) {
-                                    client.send(JSON.stringify({ type: 'req_con_update', message: msg.payload }));
+                                    client.send(JSON.stringify({ action: 'req_con_update', payload: msg.payload }));
                                 }
                             });
                         } else if (msg.channel === 'new_chat') {
-                            // console.log(msg)
-                            // console.log(client.readyState)
-                            // console.log(wss.clients)
-                            wss.clients.forEach((client) => {
-                                if (client.readyState === WebSocket.OPEN) {
-                                    const trip_id = client_trips.get(client);
-                                    if (msg.payload.trip_id === trip_id)
-                                        console.log(msg)
-                                        client.send(JSON.stringify({ type: 'new_chat', message: msg.payload }));
+                            const payload = JSON.parse(msg.payload)
+                            const tripId = payload.trip_id;
+                            // console.log(tripId) 
+                            // Send message to specific trip ID clients
+                            client_trips.forEach((id, client) => {
+                                // console.log(id === tripId)
+                                // console.log(client.readyState === WebSocket.OPEN)
+                                if (id === tripId && client.readyState === WebSocket.OPEN) {
+                                    client.send(JSON.stringify({ action: 'new_chat', payload: payload }));
                                 }
                             });
                         }
                     });
-                    
+
                     logger.log("Listening for notifications...");
                 } catch (error) {
                     logger.log('Error setting up notification listener:', error);
@@ -186,7 +121,7 @@ async function connectToDatabase() {
         await new Promise(resolve => setTimeout(resolve, RETRY_DELAY));
     }
     logger.log('Max retries reached. Could not connect to the database.');
-    process.exit(1); // Exit with failure
+    process.exit(1);
 }
 
 (async () => {
@@ -211,16 +146,6 @@ try {
 }
 
 // Define routes
-app.get('/', async (req, res) => {
-    try {
-        const responseData = { message: 'home' };
-        res.json(responseData);
-    } catch (error) {
-        logger.log(`Error in GET /: ${error.message}`);
-        res.status(500).send('Internal Server Error');
-    }
-});
-
 app.get('/helloworld', (req, res) => {
     const responseData = { message: 'Hello, World!' };
     res.json(responseData);
@@ -237,25 +162,26 @@ app.post('/sendOTP', async (req, res) => {
     }
 });
 
-// Handle uncaught exceptions
-process.on('uncaughtException', (err) => {
-    logger.log(`Uncaught Exception: ${err.message}`);
-    process.exit(1); // Exit with failure
-});
-
-// Handle unhandled promise rejections
-process.on('unhandledRejection', (reason, promise) => {
-    logger.log(`Unhandled Rejection at: ${promise}, reason: ${reason}`);
-    process.exit(1); // Exit with failure
-});
-
 // Global error handling middleware
 app.use((err, req, res, next) => {
     logger.log(`Unhandled Error: ${err.message}`);
-    res.status(err.status || 500).json({ message: err.message || 'Internal Server Error' });
+    const status = err.status || 500;
+    res.status(status).json({ message: err.message || 'Internal Server Error' });
 });
 
+// Handle uncaught exceptions and unhandled promise rejections
+process.on('uncaughtException', (err) => {
+    logger.log(`Uncaught Exception: ${err.message}`);
+    process.exit(1);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+    logger.log(`Unhandled Rejection at: ${promise}, reason: ${reason}`);
+    process.exit(1);
+});
+
+// Handle server errors
 server.on('error', (err) => {
     logger.log(`Server error: ${err.message}`);
-    process.exit(1); // Exit with failure
+    process.exit(1);
 });
